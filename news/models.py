f95bea5cf69f1482ba8fe105cff8ab3713be8d6a
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-04-01 16:03:24 -0300

    criada a model News

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
@@ -8,11 +9,33 @@ class Category(models.Model):
         return self.name
 
 
-class Users(models.Model):
-    name = models.CharField(max_length=200, blank=False)
-    email = models.EmailField(max_length=200, blank=False)
-    password = models.CharField(max_length=200, blank=False)
-    role = models.CharField(max_length=200, blank=False)
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+
+def validate_title(title):
+    if title.count(" ") < 1:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200, blank=False, validators=[validate_title]
+    )
+    content = models.TextField(max_length=None, blank=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    image = models.ImageField(upload_to="img/", blank=True)
+    categories = models.ManyToManyField(
+        Category,
+        related_name="categories",
+    )
+
+    def __str__(self):
+        return self.title
