c5bf31a21379317aefc6aad0d474e6c3b35618e2
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-04-02 16:32:21 -0300

    criada a rota api/categories atendendo ao req8

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from news.models import News, Category
 from news.forms import CreateCategoriesForm, CreateNewsForm
+from rest_framework import viewsets
+from news.serializers import CategorySerializer
 
 # Create your views here.
 
@@ -59,3 +61,8 @@ def news_form(request):
     context = {"form": form, "categories": Category.objects.all()}
 
     return render(request, "news_form.html", context)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
--- spotnews/urls.py
+++ spotnews/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
+from news.views import CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register(r"categories", CategoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("news.urls")),
+    path("api/", include(router.urls)),
 ]
 
 if settings.DEBUG:
