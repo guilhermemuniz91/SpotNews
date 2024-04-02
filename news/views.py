119652601f391cdb4dad1eafa00dd2a011e7d63d
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-04-02 14:53:33 -0300

    Criada a página de formulário de uma nova categoria atendendo aos req6

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from news.models import News
+from django.shortcuts import render, get_object_or_404, redirect
+from news.models import News, Category
+from news.forms import CreateCategoriesForm
 
 # Create your views here.
 
@@ -12,3 +13,18 @@ def index(request):
 def news_details(request, id):
     context = {"news_details": get_object_or_404(News, id=id)}
     return render(request, "news_details.html", context)
+
+
+def categories_form(request):
+    form = CreateCategoriesForm()
+
+    if request.method == "POST":
+        form = CreateCategoriesForm(request.POST)
+
+        if form.is_valid():
+            Category.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+
+    return render(request, "categories_form.html", context)
